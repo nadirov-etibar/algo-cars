7511b2643e6d169cc9f67cced421dc8ca168dac6
nadirov-etibar <nadirov.etibar@gmail.com>
2022-01-16 11:49:31 +0400

    End of Task

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let n = 6;
-let cars = [6, 2, 12, 1, 7, 9];
+let cars = [6, 2, 12, 2, 7, 6];
 let k = 3;
 
 let lengthOfRoof = [];
@@ -12,6 +12,17 @@ let sortArr = (arr) => {
             if(arr[j + 1] < arr[j]){
                 [arr[j + 1],arr[j]] = [arr[j],arr[j + 1]]
             }
+        }
+    }
+    getUnique(arr);
+    console.log(arr);
+    return arr;
+}
+
+let getUnique = (arr) => {
+    for (let i = 0; i < arr.length; i++){
+        if (arr[i] === arr[i+1]){
+            arr.splice(i, 1);
         }
     }
 
